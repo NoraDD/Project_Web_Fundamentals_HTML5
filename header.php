2abb0c7e55b196f4fb0687f1c2a3fb29e8c7eb93
NoraDD <nora2dimova@abv.bg>
2017-07-07 23:29:27 +0300

    Rename index.html to index.php, added header.php and footer.php, updated stylesheets

---
+++ header.php
@@ -0,0 +1,20 @@
+<div class="header-part">
+    <div>
+        <div>
+            <h1>Quality Cakes</h1>
+            <h3>Better Bakery</h3>
+        </div>
+        <div>
+            <h2>Quality Baked Goods</h2>
+        </div>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php?page=home">Home</a></li>
+            <li><a href="index.php?page=services">Services</a></li>
+            <li><a href="index.php?page=about">About</a></li>
+            <li><a href="index.php?page=store">Store</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+        </ul>
+    </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+    <link rel="stylesheet" href="stylesheets/style.css"/>
+    <link rel="stylesheet" href="stylesheets/header.css"/>
+    <link rel="stylesheet" href="stylesheets/main.css"/>
+    <link rel="stylesheet" href="stylesheets/footer.css"/>
+    <script src="javascripts/script.js"></script>
+</head>
+<body>
+
+<header>
+    <?php
+    include 'header.php';
+    ?>
+</header>
+
+<main>
+    <?php
+    $pages = array("home", "services", "about", "store", "contact");
+    if(isset($_GET['page']) && in_array($_GET['page'],$pages))
+    {
+        include('pages/'.$_GET['page'].'.php');
+    }else{
+        include('pages/home.php');
+    }
+    ?>
+</main>
+
+<footer>
+    <?php
+    include("footer.php");
+    ?>
+</footer>
+
+</body>
+</html>
