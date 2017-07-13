6252e2ae70149d8bd3433be5b0dfa073bdf284db
NoraDD <nora2dimova@abv.bg>
2017-07-13 13:29:07 +0300

    fixed padding, margin, width and etc. in responsive css and header and footer

--- pages/services.php
+++ pages/services.php
@@ -1,11 +1,12 @@
 <div class="container-services-upper-part">
     <div class="article-services">
-        <article>
-            <img src="images/services.jpg" />
+        <img src="images/services.jpg"/>
+        <div class="services">
             <h1>Services</h1>
             <p>This area is fully editable and you can use it to promote your products, services and facilities.
-                You could also use it to introduce your team, their roles and why they are so important to your business.</p>
-        </article>
+                You could also use it to introduce your team, their roles and why they are so important to your
+                business.</p>
+        </div>
     </div>
 </div>
 
